432e5f0ba501ef1f74447f33e54af6908ce1677f
Arham Ansari <73043239+ArmyyA@users.noreply.github.com>
2022-02-23 18:38:11 -0500

    Revert "Fix collider when rolling"
    This reverts commit dc970374b406cdf766aeda04d87a1f615176eb7b.

--- Projection/Assets/__Scripts/Controllers/Ayan1ControllerTest.cs
+++ Projection/Assets/__Scripts/Controllers/Ayan1ControllerTest.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System;
-using System.Threading.Tasks;
-
 public class Ayan1ControllerTest : MonoBehaviour
 {
 
@@ -19,8 +16,6 @@ public class Ayan1ControllerTest : MonoBehaviour
     private Vector3 velocity;
 
     public bool isGrounded;
-    public bool isRolling;
-
     public float groundCheckDistance;
     public LayerMask groundMask;
     public float gravity;
@@ -32,11 +27,8 @@ public class Ayan1ControllerTest : MonoBehaviour
     private float moveX;
 
     private Vector3 rollDirection;
-    private Vector3 rollCollider;
 
     private Rigidbody rb;
-
-    private bool rollingAnimation;
 
     // Start is called before the first frame update
     void Start()
@@ -49,27 +41,13 @@ public class Ayan1ControllerTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        
-
         Move();
         Roll();
-
     }
-
-    //IEnumerable BlinkTimer()
-    //{
-    //    yield return new WaitForSeconds(1);
-    //}
 
     private void Move()
     {
-
-
         isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
-
-  
-
 
         moveZ = Input.GetAxis("Vertical");
         moveX = Input.GetAxis("Horizontal");
@@ -132,12 +110,8 @@ public class Ayan1ControllerTest : MonoBehaviour
         anim.SetFloat("Horizontal", 0, 0.1f, Time.deltaTime);
     }
 
-    
-
     private void Walk()
     {
-
-
         moveSpeed = walkSpeed;
 
         this.anim.SetFloat("Vertical", moveZ/2, 0.1f, Time.deltaTime);
@@ -172,9 +146,7 @@ public class Ayan1ControllerTest : MonoBehaviour
         }
     }
 
-    
-
-    private async void Roll()
+    private void Roll()
     {
         rollDirection = new Vector3(moveX, 0, moveZ);
 
@@ -182,26 +154,11 @@ public class Ayan1ControllerTest : MonoBehaviour
         {
             if (Input.GetKey("q"))
             {
-
-                controller.height = 0.5f;
-                controller.center = new Vector3(0, 0.5f, 0);
-
-
                 anim.SetTrigger("Roll");
-
-
                 controller.Move(rollDirection * Time.deltaTime);
-
-
-                await Task.Delay(1600);
-
-                controller.height = 1.79f;
-                controller.center = new Vector3(0, 1, 0);
-
             }
 
         }
-
     }
 
     //private void Jump()
