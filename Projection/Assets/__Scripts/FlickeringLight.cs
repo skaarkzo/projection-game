9090bee50c2da82af2d4f4c9ef2dc86f85eda3a6
zokal <zokal@uwo.ca>
2022-03-09 23:18:49 -0500

    add wall light

--- Projection/Assets/__Scripts/FlickeringLight.cs
+++ Projection/Assets/__Scripts/FlickeringLight.cs
@@ -6,7 +6,7 @@ public class FlickeringLight : MonoBehaviour
 {
 
     public bool isFlickering = false;
-    public float timeDelay;
+    private float timeDelay;
 
     // Update is called once per frame
     void Update()
