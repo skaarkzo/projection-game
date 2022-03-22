3ba027bc98a806eccd1418ebffefd55e40c36e5e
Arham Ansari <73043239+ArmyyA@users.noreply.github.com>
2022-03-22 02:37:55 -0400

    Update Enemy AI Animations

--- Projection/Assets/__Scripts/Controllers/EnemyController.cs
+++ Projection/Assets/__Scripts/Controllers/EnemyController.cs
@@ -9,14 +9,19 @@ public class EnemyController : MonoBehaviour
     public float lookRadius = 10f;
 
     Transform target;
+    private GameObject triggeringNPC;
+    private bool triggering;
     NavMeshAgent agent;
 
-    
+    private Animator anim;
+
+
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,27 +32,54 @@ public class EnemyController : MonoBehaviour
         if (distance <= lookRadius)
         {
             agent.SetDestination(target.position);
+        }
+    }
+    
+    void FaceTarget()
+        {
+            Vector3 direction = (target.position - transform.position).normalized; // Get direction to the player
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z)); // Get a rotation to the player
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        }
 
-            if(distance <= agent.stoppingDistance)
+    public void Walk()
+    {
+        anim.SetBool("Walk", true);
+    }
+
+    public void Idle()
+    {
+        anim.SetBool("Walk", false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            triggering = true;
+            triggeringNPC = this.gameObject;
+            anim = triggeringNPC.GetComponent<Animator>();
+            if (triggering)
             {
-                // Attack the target
-
-                // Face the target
-                FaceTarget();
+                anim.SetBool("Walk", true);
             }
+            
         }
     }
 
-    void FaceTarget()
+    private void OnTriggerExit(Collider other)
     {
-        Vector3 direction = (target.position - transform.position).normalized; // Get direction to the player
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z)); // Get a rotation to the player
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        if (other.tag == "Player")
+        {
+            triggering = false;
+            triggeringNPC = null;
+            anim.SetBool("Walk", false);
+        }
     }
 
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
-    }
+    /*void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, lookRadius);
+        }*/
 }
