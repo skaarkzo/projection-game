1e166356023f42ac5b84e1db8d63a3c1859b47f1
zokal <zokal@uwo.ca>
2022-04-03 01:22:40 -0400

    Attempt to fix end mission

--- Projection/Assets/__Scripts/Level1/Dialogues/DialogueManager.cs
+++ Projection/Assets/__Scripts/Level1/Dialogues/DialogueManager.cs
@@ -1,5 +1,69 @@
+//using UnityEngine;
+//using UnityEngine.UI; 
+
+//public class DialogueManager : MonoBehaviour
+//{
+
+//    public GameObject dialogueBox;
+//    public Text dialogueText;
+
+//    public bool dialogueActive;
+
+//    public string[] dialogueLines;
+//    public int currentLine;
+
+//    public float timeLeft = 5;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        // Disables dialogue box 
+//        dialogueBox.SetActive(false);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//        if (dialogueActive)
+//        {
+//            timeLeft -= Time.deltaTime;
+//        }
+
+//        // Goes through the different lines of text
+//        if ((dialogueActive && Input.GetKeyUp(KeyCode.X)) || (dialogueActive && timeLeft <= 0))
+//        {
+//            currentLine++;
+//            timeLeft = 5;
+//        }
+
+//        // Ending the dialogue box and disabling it
+//        if (currentLine >= dialogueLines.Length)
+//        {
+//            Time.timeScale = 1;
+
+//            dialogueBox.SetActive(false);
+//            dialogueActive = false;
+//            currentLine = 0;
+//        }
+
+//        // Setting text to current line text
+//        dialogueText.text = dialogueLines[currentLine];
+
+//    }
+
+//    // Show dialogue
+//    public void ShowDialogue()
+//    {
+//        timeLeft = 5;
+//        dialogueActive = true;
+//        dialogueBox.SetActive(true);
+//    }
+
+//}
+
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -12,8 +76,6 @@ public class DialogueManager : MonoBehaviour
     public string[] dialogueLines;
     public int currentLine;
 
-    public float timeLeft = 5;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,18 +86,12 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (dialogueActive)
-        {
-            timeLeft -= Time.deltaTime;
-        }
-
         // Goes through the different lines of text
-        if ((dialogueActive && Input.GetKeyUp(KeyCode.X)) || (dialogueActive && timeLeft <= 0))
+        if (dialogueActive && Input.GetKeyUp(KeyCode.X))
         {
             currentLine++;
-            timeLeft = 5;
         }
+
 
         // Ending the dialogue box and disabling it
         if (currentLine >= dialogueLines.Length)
@@ -55,7 +111,6 @@ public class DialogueManager : MonoBehaviour
     // Show dialogue
     public void ShowDialogue()
     {
-        timeLeft = 5;
         dialogueActive = true;
         dialogueBox.SetActive(true);
     }
--- Projection/Assets/__Scripts/Level1/Dialogues/dialogueHolder.cs
+++ Projection/Assets/__Scripts/Level1/Dialogues/dialogueHolder.cs
@@ -1,3 +1,53 @@
+//using UnityEngine;
+
+//public class DialogueHolder : MonoBehaviour
+//{
+//    // Variable to hold dialogue
+//    public string dialogue;
+
+//    // Referencing other scripts 
+//    private DialogueManager dialogueManager;
+
+//    public string[] dialogueLines; // Array to store dialogue lines
+
+//    public float timeLeft = 5;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        dialogueManager = FindObjectOfType<DialogueManager>(); // Linking other scripts
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//    }
+
+//    // On trigger function
+//    private void OnTriggerStay(Collider other)
+//    {
+//        // Checks tag of object colliding to insure it is the player
+//        if (other.gameObject.name == "Player")
+//        {
+//            // Checks to see if "X" is pressed
+//            if (Input.GetKeyDown(KeyCode.X) || dialogueManager.timeLeft <= 0)
+//            {
+//                // Checks to see if dialogue is not active
+//                if (!dialogueManager.dialogueActive)
+//                {
+
+//                    dialogueManager.dialogueLines = dialogueLines; // Sets dialogue lines to text that is edited in unity
+//                    dialogueManager.currentLine = 0; // Resets current line
+//                    dialogueManager.ShowDialogue(); // Shows dialogue
+
+//                }
+
+//            }
+//        }
+//    }
+
+//}
+
 using UnityEngine;
 
 public class DialogueHolder : MonoBehaviour
@@ -10,17 +60,10 @@ public class DialogueHolder : MonoBehaviour
 
     public string[] dialogueLines; // Array to store dialogue lines
 
-    public float timeLeft = 5;
-
     // Start is called before the first frame update
     void Start()
     {
         dialogueManager = FindObjectOfType<DialogueManager>(); // Linking other scripts
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 
     // On trigger function
@@ -30,7 +73,7 @@ public class DialogueHolder : MonoBehaviour
         if (other.gameObject.name == "Player")
         {
             // Checks to see if "X" is pressed
-            if (Input.GetKeyDown(KeyCode.X) || dialogueManager.timeLeft <= 0)
+            if (Input.GetKeyDown(KeyCode.X))
             {
                 // Checks to see if dialogue is not active
                 if (!dialogueManager.dialogueActive)
